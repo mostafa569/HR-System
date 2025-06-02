fed06e29a23b353f2edfa64fbbc042eec7f5227b
Alaa Ezat <lmedo1236@gmail.com>
2025-06-02 19:55:39 +0300

    Implemented Department model

--- backend/app/Models/Department.php
+++ backend/app/Models/Department.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+// This model represents the 'departments' table in the database and manages department-related data.
 class Department extends Model
 {
-    // Your code here...
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+    
+
+    public function employers(): HasMany
+    {
+        return $this->hasMany(Employer::class, 'department_id');
+    }
 }
