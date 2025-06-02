e441b7ed57e2ac98dba3245ff503e11d528e192c
Alaa Ezat <lmedo1236@gmail.com>
2025-06-02 19:57:52 +0300

    Added API routes for Department resource

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Department\DepartmentController;
 
 Route::post('/hr/login', [HrAuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('employers', EmployerController::class);
+Route::apiResource('employers', EmployerController::class);
 Route::post('employers/{id}/attend', [EmployerController::class, 'attendEmployer']);
 Route::post('employers/{id}/leave', [EmployerController::class, 'leaveEmployer']);
 Route::apiResource('departments', DepartmentController::class);
-    Route::post('/hr/logout', [HrAuthController::class, 'logout']);
+Route::post('/hr/logout', [HrAuthController::class, 'logout']);
+   
+Route::apiResource('departments', DepartmentController::class);
 });
