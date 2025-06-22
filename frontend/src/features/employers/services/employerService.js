181fa5d6f45c908809677b8bd2a84c96343edfb4
mostafa569 <mostafa.mohamed.aboali76@gmail.com>
2025-06-22 12:57:46 +0300

    logic updated

--- frontend/src/features/employers/services/employerService.js
+++ frontend/src/features/employers/services/employerService.js
@@ -1,40 +1,4 @@
-import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000/api";
-
- 
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
- 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("userToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
- 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("userToken");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+import api from "../../auth/services/api";
 
 const getEmployers = async (
   page = 1,
@@ -130,7 +94,6 @@ const getEmployer = async (id) => {
   }
 };
 
-
 export {
   getEmployers,
   createEmployer,
@@ -140,5 +103,4 @@ export {
   leaveEmployer,
   getDepartments,
   getEmployer,
-  api,
 };
